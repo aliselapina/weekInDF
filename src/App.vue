b26a01332ded787e367d2c0491ee19778697e996
murdskristians <murdskristians@gmail.com>
2019-10-02 20:45:49 +0300

    Basis ready

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+          <router-link id="monday-nav" to="/">Monday  </router-link>
+          <router-link id="tuesday-nav" to="/tuesday">Tuesday  </router-link>
+          <router-link id="wednesday-nav" to="/wednesday">Wednesday  </router-link>
+          <router-link id="thursday-nav" to="/thursday">Thursday  </router-link>
+          <router-link id="friday-nav" to="/friday">Friday  </router-link>
+          <router-link id="saturday-nav" to="/saturday">Saturday  </router-link>
+          <router-link id="sunday-nav" to="/sunday">Sunday  </router-link>
     </div>
     <router-view/>
   </div>
---
+++ src/views/Sunday.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="friday content">
+    <img src="@/assets/friday.png" alt="friday">
+
+    <h1 class="friday-color">{{ message.title[currentLocale] }}</h1>
+    <br />
+    <p>{{ message.content[currentLocale] }}</p>
+
+    <div class="footer friday-color">
+      <select v-model="currentLocale">
+          <option
+          selected="English"
+          v-for="locale in locales"
+          v-bind:key="locale.id"
+          v-bind:value="locale.id"
+          >
+            {{locale.name}}
+          </option>
+      </select>
+      <div>Made by Alise</div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: {
+        title: {
+          en: 'Title',
+          lv: 'Virsraksts'
+        },
+        content: {
+          en: 'Some huge text',
+          lv: 'Garš teksts, kas neiederas dizainā'
+        }
+      },
+      currentLocale: 'en',
+      locales: [
+        { id: 'en', name: 'English' },
+        { id: 'lv', name: 'Latviešu' }
+      ]
+    }
+  }
+}
+</script>
